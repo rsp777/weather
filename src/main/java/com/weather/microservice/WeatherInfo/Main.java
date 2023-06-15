cd7118eab513bc73628d10b36a5461530d98719b
Ravi <Ravi@host.docker.internal>
2023-06-15 20:00:22 +0530

    LOG LEVEL DEBUG

--- src/main/java/com/weather/microservice/WeatherInfo/Main.java
+++ src/main/java/com/weather/microservice/WeatherInfo/Main.java
@@ -74,6 +74,13 @@ public class Main {
 	public void setGrnd_level(double grnd_level) {
 		this.grnd_level = grnd_level;
 	}
+
+	@Override
+	public String toString() {
+		return "Main [temp=" + temp + ", feels_like=" + feels_like + ", temp_min=" + temp_min + ", temp_max=" + temp_max
+				+ ", pressure=" + pressure + ", humidity=" + humidity + ", sea_level=" + sea_level + ", grnd_level="
+				+ grnd_level + "]";
+	}
 	
 	
 
