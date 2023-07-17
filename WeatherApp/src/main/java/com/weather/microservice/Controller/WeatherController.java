742266842aa6168677a33b9c50fb5ae9da60c126
rsp777 <raspawar007@gmail.com>
2023-07-17 10:09:39 +0530

    Added application.properties to git ignore

--- WeatherApp/src/main/java/com/weather/microservice/Controller/WeatherController.java
+++ WeatherApp/src/main/java/com/weather/microservice/Controller/WeatherController.java
@@ -3,6 +3,7 @@ package com.weather.microservice.Controller;
 import java.io.IOException;
 import java.util.logging.Logger;
 import org.apache.http.client.ClientProtocolException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.weather.microservice.config.Config;
 import com.weather.microservice.exceptions.CityNotFoundException;
 import com.weather.microservice.service.WeatherService;
 import com.weather.microservice.weatherInfo.WeatherInfo;
@@ -22,6 +24,8 @@ import com.weather.microservice.weatherInfo.WeatherInfo;
 // @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class WeatherController {
 
+	@Autowired
+	public Config  config;
 	private WeatherService weatherService;
 	private final Logger logger = Logger.getLogger(WeatherController.class.getName());
 	private WeatherInfo weatherInfo;
@@ -38,13 +42,15 @@ public class WeatherController {
 		logger.info(welcome);
 		return welcome;
 	}
-
+	
 	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping("/{city}")
 	public WeatherInfo getWeather(@PathVariable String city)
 			throws CityNotFoundException, ClientProtocolException, IOException {
-
-		weatherInfo = weatherService.getWeatherInfo(city);
+		String weatherUrl = config.getUrl();
+		String weatherKey = config.getKey();
+		String weatherParams = config.getParams();
+		weatherInfo = weatherService.getWeatherInfo(city,weatherUrl,weatherKey,weatherParams);
 		logger.info("getWeatherInfo("+city+") : "+weatherInfo);
 		
 		if (weatherInfo.getCod().equals("404")) {
--- WeatherApp/src/main/java/com/weather/microservice/service/WeatherService.java
+++ WeatherApp/src/main/java/com/weather/microservice/service/WeatherService.java
@@ -13,18 +13,17 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.weather.microservice.config.Config;
 import com.weather.microservice.weatherInfo.WeatherInfo;
 
 @Service
 public class WeatherService {
 
 	private final Logger logger = Logger.getLogger(WeatherService.class.getName());
-	private static final String API_KEY = "f777fa895ebe5cb38334176de19e5693";
-	private static final String WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather";
-	private static final String PARAMS = "%s?q=%s&appid=%s&units=metric&lang=en";
 
 	private WeatherInfo weatherInfo;
 	private final HttpClient httpClient;
@@ -38,10 +37,10 @@ public class WeatherService {
 
 	}
 
-	public WeatherInfo getWeatherInfo(String city) throws IOException, ClientProtocolException {
+	public WeatherInfo getWeatherInfo(String city, String weatherUrl, String weatherKey, String weatherParams) throws IOException, ClientProtocolException {
 		if (city != null) {
 			encodedCity = encodedCity(city);
-			String jsonWeatherResponse = jsonWeatherResponse(PARAMS, WEATHER_API_URL, encodedCity, API_KEY);
+			String jsonWeatherResponse = jsonWeatherResponse(weatherParams,weatherUrl, encodedCity, weatherKey);
 			objectMapper.setSerializationInclusion(Include.NON_NULL);
 			weatherInfo = objectMapper.readValue(jsonWeatherResponse, WeatherInfo.class);
 			logger.info("Weather fetched for " + weatherInfo.getName());
